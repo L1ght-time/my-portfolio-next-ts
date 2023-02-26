48f7b8866ffd3af9f1e593a2fec73c897e7e8901
maksymkonovalov <maksym.konovalov@clockwise.software>
2023-02-26 23:54:35 +0200

    feat: relocated logic with last locale state after refresh of page in store

--- src/components/Layout/NavBar/LanguageDropdown/LanguageDropdown.tsx
+++ src/components/Layout/NavBar/LanguageDropdown/LanguageDropdown.tsx
@@ -1,5 +1,4 @@
 import { MdKeyboardArrowDown } from 'react-icons/md';
-import { useCallback, useEffect } from 'react';
 
 import { useLocaleStore } from '@/store';
 import {
@@ -10,25 +9,11 @@ import {
   DropDownItem,
   DropDownLabel,
 } from '@/components/shared/DropDown';
-import { TLocalesKeys } from '@/i18n';
 
 import { modelLanguages } from './LanguageDropdown.constants';
 
 export const LanguageDropdown = () => {
   const { locale, setLocale } = useLocaleStore();
-
-  const handleClick = useCallback((name: TLocalesKeys) => {
-    setLocale(name);
-    localStorage.setItem('locale', name);
-  }, []);
-
-  useEffect(() => {
-    const cachedLocale = localStorage.getItem('locale');
-
-    if (cachedLocale) {
-      setLocale(cachedLocale as TLocalesKeys);
-    }
-  }, []);
 
   return (
     <DropDown>
@@ -41,7 +26,7 @@ export const LanguageDropdown = () => {
       <DropDownContent>
         {modelLanguages.map(({ id, name }) => (
           <DropDownItem key={id}>
-            <DropDownButton onClick={() => handleClick(name)}>{name.toUpperCase()}</DropDownButton>
+            <DropDownButton onClick={() => setLocale(name)}>{name.toUpperCase()}</DropDownButton>
           </DropDownItem>
         ))}
       </DropDownContent>
