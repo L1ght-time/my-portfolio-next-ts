d52b265c22deecce19aee6b20a11bf537c24527e
maksymkonovalov <maksym.konovalov@clockwise.software>
2023-03-02 14:42:37 +0200

    feat: added not found page

---
+++ src/pages/404.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { TbArrowNarrowLeft } from 'react-icons/tb';
+import { FormattedMessage } from 'react-intl';
+
+const NotFound = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      return router.push('/');
+    }, 5000);
+  }, [router]);
+
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div>
+        <button onClick={() => router.push('/')}>
+          <TbArrowNarrowLeft fontSize={24} />
+        </button>
+        <h1 className="font-medium">
+          <FormattedMessage id={'404'} />
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
