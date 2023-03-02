f51a496304442c33241184b30cb2190b31ad25a7
maksymkonovalov <maksym.konovalov@clockwise.software>
2023-03-02 14:51:01 +0200

    feat: added global constants for paths pages

---
+++ src/constants/index.ts
@@ -0,0 +1,1 @@
+export { ROUTES_PAGES } from './routesPages';
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,19 +3,21 @@ import { useRouter } from 'next/router';
 import { TbArrowNarrowLeft } from 'react-icons/tb';
 import { FormattedMessage } from 'react-intl';
 
+import { ROUTES_PAGES } from '@/constants';
+
 const NotFound = () => {
   const router = useRouter();
 
   useEffect(() => {
     setTimeout(() => {
-      return router.push('/');
+      return router.push(ROUTES_PAGES.home);
     }, 5000);
   }, [router]);
 
   return (
     <div className="flex justify-center items-center h-full">
       <div>
-        <button onClick={() => router.push('/')}>
+        <button onClick={() => router.push(ROUTES_PAGES.home)}>
           <TbArrowNarrowLeft fontSize={24} />
         </button>
         <h1 className="font-medium">
