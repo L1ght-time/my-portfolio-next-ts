66c059229730aa3f5b7575c6eb4e6643dd04ef95
L1ght-time <light.time.1995@gmail.com>
2023-03-09 21:50:59 +0200

    fix: deploy

--- src/components/pages/About/About.tsx
+++ src/components/pages/About/About.tsx
@@ -1,16 +1,15 @@
 import { NextPage } from 'next';
-import ky from 'ky';
 import { FormattedMessage } from 'react-intl';
 import useSWR from 'swr';
 
 import { IAboutData } from '@/pages/api/db/about';
 import { EmploymentHistory } from '@/components/pages/About/EmploymentHistory';
+import * as process from "process";
 
-// const fetcher = ky.get('/api/about').json;
 const fetcher = (...args: any[]) => fetch(...(args as [any, any])).then((res) => res.json());
 
 export const About: NextPage = () => {
-  const { data: about } = useSWR<IAboutData>('/api/about', fetcher);
+  const { data: about } = useSWR<IAboutData>(`${process.env.API_HOST}/about`, fetcher);
   if (!about?.employmentHistory) {
     return null;
   }
