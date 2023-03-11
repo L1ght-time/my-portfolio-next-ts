acfcb614c6898ab951ebc53b768cd09b0b7e9e66
L1ght-time <light.time.1995@gmail.com>
2023-03-11 14:12:38 +0200

    fix: pull request comments

--- src/components/layouts/MainLayout/MainLayout.tsx
+++ src/components/layouts/MainLayout/MainLayout.tsx
@@ -1,16 +1,14 @@
 import { PropsWithChildren } from 'react';
 
-import { Sidebar } from '@/components/layouts/MainLayout/Sidebar';
-import { NavBar } from '@/components/layouts/MainLayout/NavBar';
+import { NavBar } from './NavBar';
+import { Sidebar } from './Sidebar';
 
-export const MainLayout = ({ children }: PropsWithChildren) => {
-  return (
-    <div className="flex">
-      <Sidebar />
-      <div className="flex flex-col w-full">
-        <NavBar />
-        <div className="p-5 w-full h-full bg-neutral-200">{children}</div>
-      </div>
+export const MainLayout = ({ children }: PropsWithChildren) => (
+  <div className="flex">
+    <Sidebar />
+    <div className="flex flex-col w-full">
+      <NavBar />
+      <div className="p-5 w-full h-full bg-neutral-200">{children}</div>
     </div>
-  );
-};
+  </div>
+);
--- src/components/layouts/MainLayout/NavBar/NavBar.tsx
+++ src/components/layouts/MainLayout/NavBar/NavBar.tsx
@@ -1,15 +1,13 @@
-import { LanguageDropdown } from '@/components/layouts/MainLayout/NavBar/LanguageDropdown';
-import { ThemeDropdown } from '@/components/layouts/MainLayout/NavBar/ThemeDropdown';
+import { ThemeDropdown } from './ThemeDropdown';
+import { LanguageDropdown } from './LanguageDropdown';
 
-export const NavBar = () => {
-  return (
-    <div className="bg-gray-50 px-4">
-      <nav className="flex justify-end items-center w-full h-16">
-        <div className="flex justify-between gap-5">
-          <LanguageDropdown />
-          <ThemeDropdown />
-        </div>
-      </nav>
-    </div>
-  );
-};
+export const NavBar = () => (
+  <div className="bg-gray-50 px-4">
+    <nav className="flex justify-end items-center w-full h-16">
+      <div className="flex justify-between gap-5">
+        <LanguageDropdown />
+        <ThemeDropdown />
+      </div>
+    </nav>
+  </div>
+);
