0ba08e53a0e637953379ca72acdef09c3a94a522
L1ght-time <light.time.1995@gmail.com>
2023-03-13 18:40:25 +0200

    fix: pull request comments

--- src/components/pages/About/EmploymentHistory/EmploymentHistory.tsx
+++ src/components/pages/About/EmploymentHistory/EmploymentHistory.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import Image from 'next/image';
 import { FormattedDate, FormattedMessage } from 'react-intl';
 import { DateTime } from 'luxon';
@@ -12,10 +12,10 @@ export const EmploymentHistory: FC<IEmploymentHistoryProps> = ({
   position,
   period: { start, end },
 }) => {
-  const { years, months } = DateTime.fromISO(end)
-    .diff(DateTime.fromISO(start), ['years', 'months'])
-    .plus({ month: 1 })
-    .normalize();
+  const { years, months } = useMemo(
+    () => DateTime.fromISO(end).diff(DateTime.fromISO(start), ['years', 'months']).plus({ month: 1 }).normalize(),
+    [],
+  );
 
   return (
     <Card className="card w-[400px] bg-base-100 shadow-xl">
